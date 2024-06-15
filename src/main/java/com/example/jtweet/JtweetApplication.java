854e4e193e73853ade7d2abf8537da885d29a7e9
robinsonweng <robinsonweng100@gmail.com>
2024-06-15 14:10:10 +0800

    project init

---
+++ src/main/java/com/example/jtweet/JtweetApplication.java
@@ -0,0 +1,13 @@
+package com.example.jtweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jtweet/JtweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jtweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
